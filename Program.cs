6dab283057e8db77bb51392c2fc1cfd6eeed4851
arthur <arthurecomp22@gmail.com>
2025-08-21 18:12:13 -0300

    resolução do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("NOKIA");
+Nokia nokia = new Nokia("123456", "modelo 1", "1111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("--");
+
+Console.WriteLine("iphone");
+Iphone iphone = new Iphone("123456", "modelo 1", "1111111", 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
